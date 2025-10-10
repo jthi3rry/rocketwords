00de14c3f6bbe4f03cb352e6c87490c463f2db50
jthi3rry <jthi3rry@users.noreply.github.com>
2025-10-10 21:31:15 +0000

    deploy: 13af1581a4099389820f41863c912294b3f164ca

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'rocket-words-v1';
+const urlsToCache = [
+  './',
+  './assets/icons/android-chrome-192x192.png',
+  './assets/icons/android-chrome-512x512.png',
+  './assets/icons/apple-touch-icon.png',
+  './assets/icons/favicon-32x32.png',
+  './manifest.webmanifest'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
